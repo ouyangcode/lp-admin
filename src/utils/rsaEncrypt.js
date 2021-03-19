6959f50e25b71d5d64bfc4619d9fff37b0ca5a9f
cuihua <3359098681@qq.com>
2021-03-19 18:31:36 +0800

    修改公钥

--- src/utils/rsaEncrypt.js
+++ src/utils/rsaEncrypt.js
@@ -1,11 +1,12 @@
 import JSEncrypt from 'jsencrypt/bin/jsencrypt.min'
 
 // 密钥对生成 http://web.chacuo.net/netrsakeypair
-const publicKey = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCVCywD/4aEFCpauEF9LQhuo+0ITH+hPA8vaYiUEAjXRRQisQG1IdbrKuBSab7AYq7/Cu8gFWVMEEoeWm3MJShNmm8RsvGb076tm/bK5xi+3eY5ozKLbG8oc60SXwBv10SXrVGlNZgb59tRXYwJFSObefiuuyjtZGPco6zSiu4H5QIDAQAB'
-//const publicKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANL378k3RiZHWx5AfJqdH9xRNBmD9wGD\n' +
+// const publicKey = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCVCywD/4aEFCpauEF9LQhuo+0ITH+hPA8vaYiUEAjXRRQisQG1IdbrKuBSab7AYq7/Cu8gFWVMEEoeWm3MJShNmm8RsvGb076tm/bK5xi+3eY5ozKLbG8oc60SXwBv10SXrVGlNZgb59tRXYwJFSObefiuuyjtZGPco6zSiu4H5QIDAQAB'
+const publicKey = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCGI54TIucPsVlMd/IAP28KaZcW1ega69zGgRzgEzjlzoAdonX1XC7MSXjh48wId2FKwaLu4pozmqxDT/CGdMtwsNmjj1JVHCcW4qlgg4ZE/0bDVDGGIR7FpW3P6GCc8f0qkNrpiqpT1w0lEJBSTOaFjKlHF03WumjLKNoTuJYtawIDAQAB'
+// const publicKey = 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANL378k3RiZHWx5AfJqdH9xRNBmD9wGD\n' +
 //  '2iRe41HdTNF8RUhNnHit5NpMNtGL0NPTSSpPjjI1kJfVorRvaQerUgkCAwEAAQ=='
 
-const privateKey =''
+const privateKey = ''
 
 // 加密
 export function encrypt(txt) {
